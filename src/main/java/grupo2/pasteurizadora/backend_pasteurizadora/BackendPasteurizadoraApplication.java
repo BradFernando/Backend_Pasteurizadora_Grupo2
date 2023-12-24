bda5d33031a2fca35e00aa21f865d078e9bf1ca1
Bradley Fernando <bfcorro@espe.edu.ec>
2023-12-24 10:47:27 -0500

    Primer Comit

---
+++ src/main/java/grupo2/pasteurizadora/backend_pasteurizadora/BackendPasteurizadoraApplication.java
@@ -0,0 +1,13 @@
+package grupo2.pasteurizadora.backend_pasteurizadora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendPasteurizadoraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendPasteurizadoraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/grupo2/pasteurizadora/backend_pasteurizadora/BackendPasteurizadoraApplicationTests.java
@@ -0,0 +1,13 @@
+package grupo2.pasteurizadora.backend_pasteurizadora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendPasteurizadoraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
